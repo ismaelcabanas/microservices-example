58b689ce77bbb77cf15aa4149f284017b822e255
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-06-15 14:11:31 +0200

    Edge Servere implemented with Zuul

---
+++ infrastructure/zuul-server/README.md
@@ -0,0 +1,10 @@
+# Edge Server implementado por Zuul
+
+Zuul es el servidor Netflix que actúa como enrutador y balanceador de
+carga.
+
+Zuul utiliza Ribbon para balancear la carga entre microservicios.
+
+Es como exponer un punto de entrada de acceso a nuestro ecosistema de
+microservicios. En este punto podemos aplicar seguridad a nuestros microservicios.
+
---
+++ infrastructure/zuul-server/src/main/java/cabanas/garcia/ismael/microservices/infrastructure/server/edge/zuul/ZuulServerApplication.java
@@ -0,0 +1,28 @@
+package cabanas.garcia.ismael.microservices.infrastructure.server.edge.zuul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by ismaelcabanas on 15/6/17.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ZuulServerApplication.class);
+
+    public static void main(String[] args) {
+        System.setProperty("spring.jackson.serialization.INDENT_OUTPUT", "true");
+
+        LOG.info("Initializing Edge Server implemented by Zuul");
+
+        new SpringApplicationBuilder(ZuulServerApplication.class).web(true).run(args);
+
+        LOG.info("Edge Server started");
+    }
+}
