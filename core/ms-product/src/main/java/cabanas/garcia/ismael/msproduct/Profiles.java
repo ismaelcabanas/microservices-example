64d0f713134cffc982029748287338a9235a2b7a
Ismael Cabañas García <ismaelcabanas@gmail.com>
2017-06-22 09:46:44 +0200

    New class with application profiles

---
+++ core/ms-product/src/main/java/cabanas/garcia/ismael/msproduct/Profiles.java
@@ -0,0 +1,10 @@
+package cabanas.garcia.ismael.msproduct;
+
+/**
+ * Created by ismaelcabanas on 22/6/17.
+ */
+public final class Profiles {
+    private Profiles(){}
+
+    public static final String DEV = "dev";
+}
---
+++ core/ms-product/src/main/java/cabanas/garcia/ismael/msproduct/framework/configuration/KafkaConfiguration.java
@@ -0,0 +1,7 @@
+package cabanas.garcia.ismael.msproduct.framework.configuration;
+
+/**
+ * Created by ismaelcabanas on 22/6/17.
+ */
+public class KafkaConfiguration {
+}
